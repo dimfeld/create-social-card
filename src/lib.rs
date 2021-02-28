b383562bd32f76452d7a3ef806a2fb37d7c2c7f9
Daniel Imfeld <daniel@imfeld.dev>
2021-02-28 00:37:29 +0000

    Checkpoint of more advanced text layout

--- src/lib.rs
+++ src/lib.rs
@@ -5,28 +5,136 @@ use glyph_brush_layout::{
 };
 use image::{GenericImageView, ImageBuffer, Rgba};
 use serde_derive::Deserialize;
+use std::borrow::Cow;
+use std::convert::{TryFrom, TryInto};
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+pub enum Color<'a> {
+    Rgb(u8, u8, u8),
+    Rgba(u8, u8, u8, u8),
+    RgbString(Cow<'a, str>),
+}
+
+impl<'a> Default for Color<'a> {
+    fn default() -> Color<'a> {
+        Color::Rgb(0, 0, 0)
+    }
+}
+
+impl<'a> TryFrom<Color<'a>> for Rgba<u8> {
+    type Error = anyhow::Error;
+
+    fn try_from(val: Color) -> Result<Rgba<u8>> {
+        match val {
+            Color::Rgb(r, g, b) => Ok(Rgba([r, g, b, 255])),
+            Color::Rgba(r, g, b, a) => Ok(Rgba([r, g, b, a])),
+            Color::RgbString(s) => parse_color(&s),
+        }
+    }
+}
 
 #[derive(Debug)]
 pub struct OverlayOptions<'a> {
-    pub text: &'a str,
     pub background: image::DynamicImage,
-    pub font: &'a FontRef<'a>,
-    pub text_rect: &'a Rect,
-    pub min_size: f32,
-    pub max_size: f32,
-    pub color: &'a str,
-    pub shadow: Option<&'a Shadow>,
+    pub paragraphs: Vec<Paragraph<'a>>,
+    pub fonts: Vec<FontRef<'a>>,
+}
+
+#[derive(Copy, Clone, Debug, Deserialize)]
+pub enum HAlign {
+    Left,
+    Center,
+    Right,
+}
+
+impl Default for HAlign {
+    fn default() -> HAlign {
+        HAlign::Left
+    }
+}
+
+impl From<HAlign> for glyph_brush_layout::HorizontalAlign {
+    fn from(v: HAlign) -> glyph_brush_layout::HorizontalAlign {
+        match v {
+            HAlign::Left => glyph_brush_layout::HorizontalAlign::Left,
+            HAlign::Center => glyph_brush_layout::HorizontalAlign::Center,
+            HAlign::Right => glyph_brush_layout::HorizontalAlign::Right,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, Deserialize)]
+pub enum VAlign {
+    Top,
+    Center,
+    Bottom,
+}
+
+impl Default for VAlign {
+    fn default() -> VAlign {
+        VAlign::Top
+    }
+}
+
+impl From<VAlign> for glyph_brush_layout::VerticalAlign {
+    fn from(v: VAlign) -> glyph_brush_layout::VerticalAlign {
+        match v {
+            VAlign::Top => glyph_brush_layout::VerticalAlign::Top,
+            VAlign::Center => glyph_brush_layout::VerticalAlign::Center,
+            VAlign::Bottom => glyph_brush_layout::VerticalAlign::Bottom,
+        }
+    }
 }
 
 #[derive(Debug, Deserialize)]
-pub struct Shadow {
+pub struct ParagraphBorder<'a> {
+    #[serde(default)]
+    pub width: usize,
+    #[serde(default)]
+    pub color: Color<'a>,
+    pub shadow: Option<Shadow<'a>>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Paragraph<'a> {
+    pub min_size: f32,
+    pub max_size: f32,
+    pub text: Vec<Text<'a>>,
+    pub rect: Rect,
+    pub shadow: Option<Shadow<'a>>,
+    pub background: Option<(u8, u8, u8, u8)>,
+    pub border: Option<ParagraphBorder<'a>>,
+    #[serde(default)]
+    pub wrap: bool,
+    #[serde(default)]
+    pub h_align: HAlign,
+    #[serde(default)]
+    pub v_align: VAlign,
+
+    /// Text blocks in a paragraph that do not have their own color will inherit it from this color.
+    #[serde(default)]
+    pub color: Color<'a>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Text<'a> {
+    pub font_index: usize,
+    pub text: Cow<'a, str>,
+    #[serde(default)]
+    pub color: Color<'a>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Shadow<'a> {
     pub x: u32,
     pub y: u32,
     pub blur: Option<f32>,
-    pub color: Option<String>,
+    #[serde(default)]
+    pub color: Color<'a>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Clone, Copy, Debug, Deserialize)]
 pub struct Rect {
     pub top: f32,
     pub bottom: f32,
@@ -41,37 +149,53 @@ fn pt_size_to_px_scale<F: Font>(font: &F, pt_size: f32, screen_scale_factor: f32
     PxScale::from(px_per_em * height / units_per_em)
 }
 
-fn fit_glyphs(options: &OverlayOptions) -> Result<Vec<SectionGlyph>> {
-    let text_width = options.text_rect.right - options.text_rect.left;
-    let text_height = options.text_rect.bottom - options.text_rect.top;
+fn fit_glyphs(fonts: &[FontRef], options: &Paragraph) -> Result<Vec<SectionGlyph>> {
+    let text_width = options.rect.right - options.rect.left;
+    let text_height = options.rect.bottom - options.rect.top;
 
     let geometry = SectionGeometry {
-        screen_position: (options.text_rect.left, options.text_rect.top),
+        screen_position: (options.rect.left, options.rect.top),
         bounds: (text_width, text_height),
     };
 
-    let layout = Layout::Wrap {
-        line_breaker: glyph_brush_layout::BuiltInLineBreaker::UnicodeLineBreaker,
-        h_align: glyph_brush_layout::HorizontalAlign::Left,
-        v_align: glyph_brush_layout::VerticalAlign::Top,
+    let layout = if options.wrap {
+        Layout::Wrap {
+            line_breaker: glyph_brush_layout::BuiltInLineBreaker::UnicodeLineBreaker,
+            h_align: options.h_align.into(),
+            v_align: options.v_align.into(),
+        }
+    } else {
+        Layout::SingleLine {
+            line_breaker: glyph_brush_layout::BuiltInLineBreaker::UnicodeLineBreaker,
+            h_align: options.h_align.into(),
+            v_align: options.v_align.into(),
+        }
     };
 
     let mut font_size = options.max_size;
 
+    let mut sections = options
+        .text
+        .iter()
+        .map(|t| SectionText {
+            text: &t.text,
+            font_id: FontId(t.font_index),
+            scale: PxScale::from(0.0), // This will be filled in later
+        })
+        .collect::<Vec<_>>();
+
     while font_size > options.min_size {
         // println!("Trying font size {font_size}", font_size = font_size);
-        let section_text = SectionText {
-            text: options.text,
-            font_id: FontId(0),
-            scale: pt_size_to_px_scale(options.font, font_size, 1.0),
-        };
+        for i in sections.iter_mut() {
+            i.scale = pt_size_to_px_scale(&fonts[i.font_id], font_size, 1.0);
+        }
 
-        let glyphs = layout.calculate_glyphs(&[options.font], &geometry, &[section_text]);
+        let glyphs = layout.calculate_glyphs(fonts, &geometry, &sections);
 
         let last_glyph = glyphs.last().unwrap();
         println!("size {}, {:?}", font_size, last_glyph);
         let text_bottom = last_glyph.glyph.position.y + last_glyph.glyph.scale.y;
-        if text_bottom > options.text_rect.bottom {
+        if text_bottom > options.rect.bottom {
             font_size -= 4.0;
         } else {
             println!("Chose font size {}", font_size);
@@ -88,13 +212,29 @@ fn blend(a: u8, b: u8, alpha: f32) -> u8 {
     (a + b) as u8
 }
 
-fn parse_color(color: &str) -> Result<(u8, u8, u8)> {
-    let color = u32::from_str_radix(color, 16).context("color")?;
-    let red: u8 = ((color >> 16) & 0xFF) as u8;
-    let green: u8 = ((color >> 8) & 0xFF) as u8;
-    let blue: u8 = (color & 0xFF) as u8;
+fn parse_color(color: &str) -> Result<Rgba<u8>> {
+    let hex = if color.starts_with('#') {
+        &color[1..]
+    } else {
+        color
+    };
 
-    Ok((red, green, blue))
+    let mut color = u32::from_str_radix(color, 16).context("color")?;
+    let mut alpha: u8 = 255;
+    if hex.len() == 8 {
+        alpha = (color & 0xFF) as u8;
+        color = color >> 8;
+    }
+
+    if hex.len() == 6 {
+        let red: u8 = ((color >> 16) & 0xFF) as u8;
+        let green: u8 = ((color >> 8) & 0xFF) as u8;
+        let blue: u8 = ((color) & 0xFF) as u8;
+
+        Ok(Rgba([red, green, blue, alpha]))
+    } else {
+        Err(anyhow!("Color must be 6 or 8 hex digits"))
+    }
 }
 
 // TODO Proper library errors instead of anyhow
@@ -103,73 +243,79 @@ pub fn overlay_text(options: &OverlayOptions) -> Result<ImageBuffer<Rgba<u8>, Ve
     let (width, height) = bg.dimensions();
     let width_f32 = width as f32;
     let height_f32 = height as f32;
-    if options.text_rect.left > width_f32
-        || options.text_rect.right > width_f32
-        || options.text_rect.top > height_f32
-        || options.text_rect.bottom > height_f32
-    {
-        return Err(anyhow!(
-            "Text rect {rect:?} does not fit in image of size {width}x{height}",
-            rect = options.text_rect,
-            width = width,
-            height = height
-        ));
-    } else if options.text_rect.left >= options.text_rect.right
-        || options.text_rect.top > options.text_rect.bottom
-    {
-        return Err(anyhow!("text_rect must not have a negative size"));
-    }
 
-    let glyphs = fit_glyphs(options)?;
-
-    let (red, green, blue) = parse_color(options.color)?;
-
-    let (shadow_red, shadow_green, shadow_blue) = options
-        .shadow
-        .and_then(|s| s.color.as_ref())
-        .map(|s| parse_color(s))
-        .transpose()?
-        .unwrap_or((128, 128, 128));
-
-    let mut text_image = image::RgbaImage::new(width, height);
-    let mut shadow_image = options
-        .shadow
-        .map(|s| (s, image::RgbaImage::new(width, height)));
-
-    for glyph in glyphs {
-        // println!("{:?}", glyph);
-        if let Some(g) = options.font.outline_glyph(glyph.glyph) {
-            // println!("{:?}", g.px_bounds());
-            let r = g.px_bounds();
-            let x_base = r.min.x as u32;
-            let y_base = r.min.y as u32;
-            g.draw(|x, y, c| {
-                let alpha = (c * 255.0) as u8;
-                // println!("{x}, {y}, {c}", x = x, y = y, c = c);
-                let pixel = image::Rgba([red, green, blue, alpha]);
-                text_image.put_pixel(x_base + x, y_base + y, pixel);
-
-                if let Some((s, i)) = shadow_image.as_mut() {
-                    let shadow_x = x_base + x + s.x;
-                    let shadow_y = y_base + y + s.y;
-                    if i.in_bounds(shadow_x, shadow_y) {
-                        let pixel = image::Rgba([shadow_red, shadow_green, shadow_blue, alpha]);
-                        i.put_pixel(shadow_x, shadow_y, pixel);
-                    }
-                }
-            })
+    for paragraph in &options.paragraphs {
+        if paragraph.rect.left > width_f32
+            || paragraph.rect.right > width_f32
+            || paragraph.rect.top > height_f32
+            || paragraph.rect.bottom > height_f32
+        {
+            return Err(anyhow!(
+                "Text rect {rect:?} does not fit in image of size {width}x{height}",
+                rect = paragraph.rect,
+                width = width,
+                height = height
+            ));
+        } else if paragraph.rect.left >= paragraph.rect.right
+            || paragraph.rect.top > paragraph.rect.bottom
+        {
+            return Err(anyhow!("rect must not have a negative size"));
         }
-    }
 
-    if let Some((s, i)) = shadow_image {
-        let i = s
-            .blur
-            .map(|blur_sigma| image::imageops::blur(&i, blur_sigma))
-            .unwrap_or(i);
-        image::imageops::overlay(&mut bg, &i, 0, 0);
-    }
+        let glyphs = fit_glyphs(&options.fonts, paragraph)?;
+        // TODO Make all this actually work on a per-paragraph basis
 
-    image::imageops::overlay(&mut bg, &text_image, 0, 0);
+        let shadow_color = paragraph
+            .shadow
+            .map(|s| s.color.try_into())
+            .transpose()?
+            .unwrap_or_else(|| Rgba([128, 128, 128, 255]));
+
+        let mut text_image = image::RgbaImage::new(width, height);
+        let mut shadow_image = paragraph
+            .shadow
+            .map(|s| (s, image::RgbaImage::new(width, height)));
+
+        for glyph in glyphs {
+            // println!("{:?}", glyph);
+            let glyph_font = &options.fonts.as_slice()[glyph.font_id];
+            if let Some(g) = glyph_font.outline_glyph(glyph.glyph) {
+                // println!("{:?}", g.px_bounds());
+                let r = g.px_bounds();
+                let x_base = r.min.x as u32;
+                let y_base = r.min.y as u32;
+                g.draw(|x, y, c| {
+                    let alpha = (c * 255.0) as u8;
+                    // println!("{x}, {y}, {c}", x = x, y = y, c = c);
+                    let mut pixel = image::Rgba([red, green, blue, alpha]);
+                    text_image.put_pixel(x_base + x, y_base + y, pixel);
+
+                    if let Some((s, i)) = shadow_image.as_mut() {
+                        let shadow_x = x_base + x + s.x;
+                        let shadow_y = y_base + y + s.y;
+                        if i.in_bounds(shadow_x, shadow_y) {
+                            let mut pixel = shadow_color.clone();
+                            if c < 1.0 {
+                                pixel[3] = ((pixel[3] as f32) * c) as u8;
+                            }
+
+                            i.put_pixel(shadow_x, shadow_y, pixel);
+                        }
+                    }
+                })
+            }
+        }
+
+        if let Some((s, i)) = shadow_image {
+            let i = s
+                .blur
+                .map(|blur_sigma| image::imageops::blur(&i, blur_sigma))
+                .unwrap_or(i);
+            image::imageops::overlay(&mut bg, &i, 0, 0);
+        }
+
+        image::imageops::overlay(&mut bg, &text_image, 0, 0);
+    }
 
     Ok(bg)
 }
